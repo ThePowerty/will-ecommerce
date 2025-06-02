ab62ad4ba3f05f8ffba6ea889760a24aaf024a2f
ThePowerty <w.infanzon.98@gmail.com>
2025-06-02 19:57:57 +0200

    Update Permissions in SecurityConfig

--- src/main/java/com/williams/will_ecommerce/auth/security/config/SecurityConfig.java
+++ src/main/java/com/williams/will_ecommerce/auth/security/config/SecurityConfig.java
@@ -38,19 +38,55 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement( sessionMangaConfig -> sessionMangaConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(sessionMangaConfig -> sessionMangaConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorize -> {
                             authorize.requestMatchers("/auth/**").permitAll();
-                            authorize.requestMatchers(HttpMethod.PUT,"/user/permission/**").hasAuthority(Permission.MANAGE_ROLES.name());
-                            authorize.requestMatchers(HttpMethod.DELETE,"/user/**").hasAuthority(Permission.DELETE_USERS.name());
+
+                            // UserController
+                            authorize.requestMatchers(HttpMethod.GET, "/user/{id}").hasAnyAuthority(Permission.READ_USERS.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/user").hasAnyAuthority(Permission.READ_USERS.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.DELETE, "/user/{id}").hasAnyAuthority(Permission.DELETE_USERS.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/user/count").hasAnyAuthority(Permission.READ_USERS.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/user/role/{role}").hasAnyAuthority(Permission.READ_USERS.name(), Permission.MANAGE_ROLES.name());
+
+                            // CarController
+                            authorize.requestMatchers(HttpMethod.GET, "/cars").hasAnyAuthority(Permission.READ_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/cars/{id}").hasAnyAuthority(Permission.READ_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/cars/doors/{doors}").hasAnyAuthority(Permission.READ_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/cars/manufacturer/{manufacturer}").hasAnyAuthority(Permission.READ_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/cars/model/{model}").hasAnyAuthority(Permission.READ_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/cars/fuelType/{fuelType}").hasAnyAuthority(Permission.READ_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/cars/status/{status}").hasAnyAuthority(Permission.READ_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/cars/seats/{seats}").hasAnyAuthority(Permission.READ_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.POST, "/cars").hasAnyAuthority(Permission.CREATE_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.DELETE, "/cars/{id}").hasAnyAuthority(Permission.DELETE_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.DELETE, "/cars").hasAnyAuthority(Permission.DELETE_VEHICLES.name(), Permission.MANAGE_ROLES.name());
+
+                            // EmployeeController
+                            authorize.requestMatchers(HttpMethod.POST, "/employee").hasAnyAuthority(Permission.CREATE_EMPLOYEES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.PUT, "/employee/{id}").hasAnyAuthority(Permission.UPDATE_EMPLOYEES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.PUT, "/employee/permission/{id}/{role}").hasAnyAuthority(Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/employee/{id}").hasAnyAuthority(Permission.READ_EMPLOYEES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/employee").hasAnyAuthority(Permission.READ_EMPLOYEES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.DELETE, "/employee/{id}").hasAnyAuthority(Permission.DELETE_EMPLOYEES.name(), Permission.MANAGE_ROLES.name());
+
+                            // SalesController
+                            authorize.requestMatchers(HttpMethod.GET, "/sales").hasAnyAuthority(Permission.READ_SALES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/sales/{id}").hasAnyAuthority(Permission.READ_SALES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.GET, "/sales/status/{saleStatus}").hasAnyAuthority(Permission.READ_SALES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.POST, "/sales").hasAnyAuthority(Permission.CREATE_SALES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.DELETE, "/sales/{id}").hasAnyAuthority(Permission.DELETE_SALES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.DELETE, "/sales").hasAnyAuthority(Permission.DELETE_SALES.name(), Permission.MANAGE_ROLES.name());
+                            authorize.requestMatchers(HttpMethod.DELETE, "/sales/many/{ids}").hasAnyAuthority(Permission.DELETE_SALES.name(), Permission.MANAGE_ROLES.name());
+
                             authorize.anyRequest().authenticated();
                         }
                 )
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
-                .exceptionHandling( exceptionHandling -> exceptionHandling
+                .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(authEntryPoint)
                         .accessDeniedHandler(accessDeniedHandler));
         return http.build();
